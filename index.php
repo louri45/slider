647f7e9c534d1e028252aaaa0082a4528688407f
louri45 <l.richard45@gmail.com>
2016-11-14 14:50:01 +0100

    kjoabziud

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>jQuery</title>
+	<link rel="stylesheet" href="custom.css">
+	<script src="jquery-3.1.0.min.js"></script>
+	<script src="jquery.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+	<div id="carousel">
+
+		<div id="slides">
+			<ul>
+				<li><img src="http://img0.mxstatic.com/wallpapers/8c546107651e1a7916de74a08b6e0f1c_large.jpeg" alt="img1"></li>
+				<li><img src="http://img1.mxstatic.com/wallpapers/195a8b0057a8c0d94a0b1913d0925b9e_large.jpeg" alt="img2"></li>
+				<li><img src="http://iwallpapers2.free.fr/images/Paysages_3D/3D_creation_paysage_1920x1080.jpg" alt="img3"></li>
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</div>
+	<div id="buttons">
+		<a href="#" id="prev"><img src="img/fleche-de-virage-a-gauche-icone-6618-32.png" alt=""></a>
+		<a href="#" id="next"><img src="img/fleche-de-virage-a-droite-icone-7985-32.png" alt=""></a>
+	</div>
+</body>
+</html>
